9121e99a18921bccd875abb9f582fbf5860a3cad
if76evax <harikrishnan.dilli.ravi@fau.dex>
2024-11-13 20:59:04 +0100

    pipline updated

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
-pip install -r project/requirements.txt
-python project/main.py
+
+# Exit
+set -e
+
+try_command() {
+    "$@" || return 1
+}
+
+# Multi agent support for pack insatllation
+if try_command pip install -r project/requirements.txt; then
+    echo "used windows runner"
+elif try_command pip3 install -r project/requirements.txt; then
+    echo "used linux/mac runner"
+else
+    echo "Failed"
+    exit 1
+fi
+
+# Multi agent support for Runner
+if try_command python project/main.py; then
+    echo "used windows runner"
+elif try_command python3 project/main.py; then
+    echo "used linux/mac runner"
+else
+    echo "Failed"
+    exit 1
+fi
